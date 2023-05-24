b1c8f6a9b2c3e8a1e38ac64090b0e26e3eadb599
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-24 12:49:02 +0300

    Got values from the environment variables

--- connection/database.py
+++ connection/database.py
@@ -1,1 +1,9 @@
 """The database file"""
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+MYSQL_USER = os.environ.get("MYSQL_USER")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+MYSQL_DB = os.environ.get("MYSQL_DB")
