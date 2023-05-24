4c884d667b00565ab76cd3e2121513b204a7b85f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-24 12:54:28 +0300

    Updated the db connection string

--- connection/database.py
+++ connection/database.py
@@ -1,9 +1,12 @@
 """The database file"""
 import os
 from dotenv import load_dotenv, find_dotenv
+from sqlalchemy import create_engine
 
 load_dotenv(find_dotenv())
 
 MYSQL_USER = os.environ.get("MYSQL_USER")
 MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
 MYSQL_DB = os.environ.get("MYSQL_DB")
+
+DATABASE_URL = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@localhost:3306/{MYSQL_DB}'
