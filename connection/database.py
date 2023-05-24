3dad4935a199e996ffd0efff1c5c0ce980cad58c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-24 12:55:46 +0300

    Created an instance of MetaData

--- connection/database.py
+++ connection/database.py
@@ -1,7 +1,7 @@
 """The database file"""
 import os
 from dotenv import load_dotenv, find_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 
 load_dotenv(find_dotenv())
 
@@ -10,3 +10,5 @@ MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
 MYSQL_DB = os.environ.get("MYSQL_DB")
 
 DATABASE_URL = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@localhost:3306/{MYSQL_DB}'
+
+metadata = MetaData()
