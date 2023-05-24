1729c88cc662767af4addbd0a37d685416bd9d62
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-24 13:01:57 +0300

    Created the books model

--- models/books_model.py
+++ models/books_model.py
@@ -1,1 +1,11 @@
 """Created the model file for the books data"""
+from sqlalchemy import Table, Column, Integer, String
+from connection.database import metadata
+
+books = Table(
+    "books",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String),
+    Column("Author", String)
+)
